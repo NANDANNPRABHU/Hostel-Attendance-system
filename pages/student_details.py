244b1be4889e3c9755f38ce92394552aa4d084ee
NANDANNPRABHU <113708691+NANDANNPRABHU@users.noreply.github.com>
2023-11-08 22:47:47 +0530

    Add files via upload

---
+++ pages/student_details.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import mysql.connector
+import pandas as pd
+
+def app():
+    # Function to create MySQL connection
+    def create_conn():
+        conn = mysql.connector.connect(
+            host='localhost',
+            user='warden',
+            password='warden',
+            database='myproject'
+        )
+        return conn
+
+    # Function to call stored procedures
+    def call_procedure(procedure_name, srn):
+        conn = create_conn()
+        cursor = conn.cursor()
+
+        try:
+            cursor.callproc(procedure_name, [srn])
+            details = cursor.stored_results()
+
+            # Get column names
+            column_names = [i[0] for i in details.description]
+
+            # Create a DataFrame from the details
+            df = pd.DataFrame(details.fetchall(), columns=column_names)
+            conn.close()
+
+            return df
+        except mysql.connector.Error as e:
+            st.error(f"Error: {e}")
+            return pd.DataFrame()
+
+    # Streamlit app
+    st.title('Details')
+
+    # Input for SRN
+    srn = st.text_input('Enter SRN')
+
+    # Button to fetch and display student details
+    if st.button('Get Student Details'):
+        if srn:
+            df = call_procedure('GetStudentInformation', srn)
+            if not df.empty:
+                st.write('**Student Details:**')
+                st.table(df)
+            else:
+                st.write('No details found.')
+        else:
+            st.write('Please enter a SRN.')
+
+    # Button to fetch and display parent details
+    if st.button('Get Parent Details'):
+        if srn:
+            df = call_procedure('GetParentInformation', srn)
+            if not df.empty:
+                st.write('**Parent Details:**')
+                st.table(df)
+            else:
+                st.write('No details found.')
+        else:
+            st.write('Please enter a SRN.')
+
+    # Button to fetch and display guardian details
+    if st.button('Get Guardian Details'):
+        if srn:
+            df = call_procedure('GetGuardianInformation', srn)
+            if not df.empty:
+                st.write('**Guardian Details:**')
+                st.table(df)
+            else:
+                st.write('No details found.')
+        else:
+            st.write('Please enter a SRN.')
+
+if __name__ == '__main__':
+    app()
